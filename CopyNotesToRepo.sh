196d82c54731d956b59797a3f6bf0fcf83a80d1d
root <root@OrtegasLaptop760.localdomain>
2022-03-21 19:56:17 -0400

    I am a script that is Updating Estevan's Notes

--- CopyNotesToRepo.sh
+++ CopyNotesToRepo.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
-# The CopyNotesToRepo script was created by Estevan Ortega.
-# This program copies note text files from desktop to repository to be then pushed to Git.
-
-# Notes_on_Prog_Lang_Syntax_by_Estevan
+# The CopyNotesToRepo script was created to copy
+# text files from desktop to the Programming_Notes_by_Estevan folder repository
+# to then be pushed to Git.
 
 cd ..
+
+# moves to Desktop to copy notes to repository
 
 cp Bash_Commands.txt Git_Commands.txt Python_Programming_Syntax.txt Java_Programming_Syntax.txt x86_Assembly_Programming_Notes.txt CopyNotesToRepo.sh Programming_Notes_by_Estevan
 
